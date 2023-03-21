20812d563722de989f15faedf86f74638f89c300
HENOK MEKONNEN <Henokme@ethiopianairlines.com>
2023-03-21 16:16:00 +0300

    Progress on tumbnail

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-$(function () {
+
+  $(function () {
          $(".like").click(function () {
              var input = $(this).find('.qty1');
              input.val(parseInt(input.val())+ 1);
@@ -7,20 +8,6 @@ $(function () {
      
   });
 
-  $('.card-wrapper').click(function() {
-    clickToExapndCards($(this));
-  });
-  
-  function clickToExapndCards($obj){
-    var clickedElement = $obj;
-    if (clickedElement.hasClass('expanded')) {
-      clickedElement.find('.card-text').hide('slow');
-      clickedElement.removeClass('expanded');
-    } else {
-       clickedElement.find('.card-text').show('slow');
-      clickedElement.addClass('expanded');
-    }
-    
-  };
+
   
   
